91e703bc4f0794e9690bd5346045cf8e6bf2c37f
Rafał Panasewicz <rafal.panasewicz@misio.tech>
2021-01-03 19:10:08 +0100

    Add enums controller

---
+++ src/iCBM.WebApi/Controllers/EnumsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using iCBM.Application.Dtos;
+using iCBM.Domain.Enums;
+using Misio.Domain.Types;
+
+namespace iCBM.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnumsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public EnumsController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet("colors")]
+        public IActionResult GetColors()
+        {
+            return Ok(_mapper.Map<ColorDto>(Enumeration.GetAll<Color>()));
+        }
+    }
+}
